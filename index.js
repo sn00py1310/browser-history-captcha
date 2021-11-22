477a43250d2ccbff088371f01cb32d5d2a92963a
sn00py1310 <61204194+sn00py1310@users.noreply.github.com>
2021-11-22 11:43:35 +0100

    Test

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function setup() {
         "//instagram.com",
         "//facebook.com",
         "//www.youtube.com/watch?v=dQw4w9WgXcQ",
-        "//www.youtube.com/watch?v=dQw4w9WgXcQq"
+        "//de.pornhub.com",
+        "//xhamster.com"
         ]
     
     let words_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
